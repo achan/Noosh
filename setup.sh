785481fe04eb1d299c5ba859dd80f793f58476a8
Amos Chan <amosschan@gmail.com>
2017-05-19 00:49:50 -0400

    Share Noosh scheme

--- setup.sh
+++ setup.sh
@@ -3,6 +3,4 @@
 carthage checkout
 mv ~/build/achan/Noosh/Carthage/Checkouts/reddift/test/test_config.json{.sample,}
 mv ~/build/achan/Noosh/Carthage/Checkouts/reddift/application/reddift_config.json{.sample,}
-ls -al  ~/build/achan/Noosh/Carthage/Checkouts/reddift/test/test_config.json{.sample,}
-ls -al  ~/build/achan/Noosh/Carthage/Checkouts/reddift/application/reddift_config.json{.sample,}
 carthage build --platform ios
